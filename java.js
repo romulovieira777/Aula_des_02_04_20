861be39d119323bf1e49968405e8747a6352b64e
Romulo Vieira <romulo.vieira777@gmail.com>
2020-04-02 22:09:05 -0300

    Aula Dev

---
+++ java.js
@@ -0,0 +1,82 @@
+/*alert("Aula de JavaScript");*/
+/*var nome = "";
+var num = 0;
+//comentário 1 linha
+
+if(condição){
+    bloco de comandos;
+}
+else if(condição){
+    bloco de comandos;
+}
+else{
+
+}*/
+/*var cont = 0;
+while(cont <= 2){
+    alert("Rômulo Vieira");
+    //cont = cont + 1;
+    cont++; 
+}
+
+do{
+    alert("Dentro da Estrutura DO");
+    alert(cont);
+    cont++;
+}while(cont <= 8)
+
+for (var x = 0; x <= 2; x++){
+    alert("Rômulo Vieira " + 2);
+}
+
+var y = 2;
+
+for (var x = 0; x < 10; x+= 2){
+    alert(x);
+}
+
+var y = 2;
+
+for (var x = 10; x >= 0; x--){
+    alert(x);
+}*/
+
+/*var num = 0;
+var i = 5;
+
+while(num <= 10){
+    alert(i + " x " + num + " = " + i * num);
+    num ++
+}
+
+for(var x = 0; x <= 10; x++){
+    alert(i + " x " + x + " = " + i * x)
+}*/
+
+/*function somar(){
+    alert(2 + 3);
+}
+somar();
+
+function subtrair(x, y){
+    alert(x - y);
+}
+subtrair(10, 3);*/
+
+/*function somar(){
+    return 2 + 3;
+}
+function subtrair(x, y){
+    alert(x - y);
+}
+function mensagem(m){
+    var valor = somar();
+    subtrair(18, valor);
+    alert("Fim da execução. " + m);
+}
+mensagem("02/04/2020");*/
+
+for(var a = 0; a <= 10; a++){
+    document.write(a + "<br/>");
+}  
+
